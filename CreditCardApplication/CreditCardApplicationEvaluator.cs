6ee3424ba0a469b7790f133a9d7f312e09033ab5
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2021-11-21 00:48:23 +1100

    Added evaluator class

---
+++ CreditCardApplication/CreditCardApplicationEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CreditCardApplication
+{
+    public class CreditCardApplicationEvaluator
+    {
+        private const int AutoReferralMaxAge = 20;
+        private const int HighIncomeThreshold = 100_000;
+        private const int LowIncomeThreshold = 20_000;
+
+        public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
+        {
+            if (application.GrossAnnualIncome >= HighIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoAccepted;
+            }
+
+            if (application.Age >= AutoReferralMaxAge)
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
+            }
+
+            if (application.GrossAnnualIncome < LowIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoDeclined;
+            }
+
+            return CreditCardApplicationDecision.ReferredToHuman;
+        }
+    }
+}
