3f8a856f0c74562a08cbbdd5179f620dcfd5acef
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2021-11-21 20:37:45 +1100

    Linq to Mock syntax

--- CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -395,5 +395,27 @@ namespace CreditCardApplication.Tests
 
             Assert.Equal(CreditCardApplicationDecision.ReferredToHumanFraudRisk, decision);
         }
+
+        [Fact]
+        public void LinqToMocks()
+        {
+            /*var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+            mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("OK");
+            mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);*/
+
+            IFrequentFlyerNumberValidator mockValidator = Mock.Of<IFrequentFlyerNumberValidator>
+                (
+                    validator =>
+                        validator.ServiceInformation.License.LicenseKey == "OK" &&
+                        validator.IsValid(It.IsAny<string>()) == true
+                );
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator);
+
+            var application = new CreditCardApplication { Age = 25 };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+        }
     }
 }
